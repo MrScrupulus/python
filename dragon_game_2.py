3748c245b3a7a142915969aa5be8c4b72d396c28
Emile D <mr.scrupulus@gmail.com>
2025-06-03 23:03:36 +0200

    add name

--- dragon_game_2.py
+++ dragon_game_2.py
@@ -1,10 +1,14 @@
+nom =input("Tapez votre nom : ")
+print("Bonjour "+ nom)
+
+
 print("Tu te trouves dans une pièce obscure d'un mystérieux château.")
 print("Tu dois choisir entre 4 portes pour continuer ton aventure.")
 
 gamerChoice = input("Choisis 1,2, 3 ou 4...")
 
 if gamerChoice == "1":
-    print("Tu te retrouves face à un dragon endormi, ton haleine fetide le réveille !")
+    print(nom +", tu te retrouves face à un dragon endormi, ton haleine fetide le réveille !")
     print("Tu es mort ! Brosse-toi les dents la prochaine fois !")
 elif gamerChoice == "2":
     print("Derrière la porte se trouve la queue d'un dragon.")
@@ -13,36 +17,37 @@ elif gamerChoice == "2":
     print("2) Fermer la porte")
     dragonChoice = input("Entre 1 ou 2... ")
     if dragonChoice == "1":
-        print("Tu as réveillé le dragon ! Tu es mort !")
+        print(nom+", tu as réveillé le dragon ! Tu es mort !")
         print("Brosse-toi les dents bordel !")
     elif dragonChoice == "2":
         print("Tu as fermé la porte et tu peux retenter ta chance ! (c'est la 4, si tu es un peu neuneu)")
         gamerChoice = input("Choisis 1,2, 3 ou 4... ")
         if gamerChoice == "1":
-            print("Tu te retrouves face à un dragon endormi, ton haleine fetide le réveille !")
+            print(nom+", tu te retrouves face à un dragon endormi, ton haleine fetide le réveille !")
             print("Tu es mort ! Brosse-toi les dents la prochaine fois !") 
         elif gamerChoice == "2":
-            print("T'es pas futfut, tu es revenu dans la même pièce !")
+            print("T'es pas futfut " +nom+ ", tu es revenu dans la même pièce !")
             print("Et le dragon est toujours là !")
             print("Laisse toi bouffer, tu le mérites !")
             
         elif gamerChoice == "3":
             print("Tu as trouvé un sac de croquette !")
-            print("Bravo, si on veut... tu peux toujours essayer de les donner au dragon !")
+            print("Bravo" +nom+ ", si on veut... tu peux toujours essayer de les donner au dragon !")
         elif gamerChoice == "4":
             print("Tu as fait toutes les portes, casse-toi de mon château maintenant !")
             print("Et n'oublie pas le chat !!! ")
-            print("Oui il y avait un chat")
+            print("Oui " +nom+ ", il y avait un chat")
+            print("Je parie que tu as donné les croquettes au dragon !")
         else:
             print("Apparement tu ne sais pas ce qu'est un chiffre entre 1 et 2.")
 elif gamerChoice == "3":
     print("Tu as trouvé un sac de croquette !")
-    print("Bravo, si on veut... tu peux toujours essayer de les donner au dragon !")
+    print("Bravo " +nom+ ", si on veut... tu peux toujours essayer de les donner au dragon !")
 elif gamerChoice == "4":
     print("Tu as fait toutes les portes, casse-toi de mon château maintenant !")
     print("Et n'oublie pas le chat !!! ")
-    print("Oui il y avait un chat")
+    print("Oui " +nom+ ", il y avait un chat")
     print("Je parie que tu as donné les croquettes au dragon !")
 else:
-    print("Apparement tu ne sais pas ce qu'est un chiffre entre 1 et 4.")
+    print("Apparement " +nom+ ", tu ne sais pas ce qu'est un chiffre entre 1 et 4.")
 
